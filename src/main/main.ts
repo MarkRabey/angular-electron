7d25d3cecf3920937f45db06a9d46f6f960d6c3b
Mark Rabey <mark@markrabey.com>
2018-04-19 14:29:37 -0400

    update build process

--- src/main/main.ts
+++ src/main/main.ts
@@ -20,7 +20,7 @@ const createWindow = async () => {
     mainWindow.webContents.openDevTools({ mode: 'detach' });
   } else {
     mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
+      pathname: path.join(__dirname, '..', 'renderer', 'index.html'),
       protocol: "file:",
       slashes: true,
     }));
